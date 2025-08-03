9cdabab78d1181633c829cf567fc9929a935d572
Sowu Kelly <sowukelly67@gmail.com>
2025-08-03 19:18:28 +0200

    posey_backend

--- posey/posey/settings.py
+++ posey/posey/settings.py
@@ -7,11 +7,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)kozn7!cqgydgx+2ow@5)eiby@&2=uyk-1g!84cbf4s2)-=zqs'
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "a321a3f46fbb.ngrok-free.app", "posey-frontend.vercel.app", "127.0.0.1:8000"]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "http://127.0.0.1:8000",
+    "https://a321a3f46fbb.ngrok-free.app",
+    "https://posey-frontend.vercel.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
@@ -101,10 +104,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.AllowAny',
-    ),
+    )
 }
 
 SIMPLE_JWT = {
