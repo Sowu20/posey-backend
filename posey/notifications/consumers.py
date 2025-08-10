d2bde354a2791413550646aa9e2fe23fc06f77d4
Sowu Kelly <sowukelly67@gmail.com>
2025-08-11 00:10:33 +0200

    posey_backend

--- posey/notifications/consumers.py
+++ posey/notifications/consumers.py
@@ -1,8 +1,8 @@
 import json 
 from channels.generic.websocket import AsyncWebsocketConsumer 
 
-class  NotificationConsumer ( AsyncWebsocketConsumer ): 
-    async  def  connect ( self ): 
+class NotificationConsumer(AsyncWebsocketConsumer): 
+    async def connect(self): 
         self.group_name = f"user_{self.scope['user'].id}"
 
         # Rejoindre le groupe de notification utilisateur 
@@ -13,15 +13,15 @@ class  NotificationConsumer ( AsyncWebsocketConsumer ):
 
         await self.accept() 
 
-    async  def  disconnect ( self, close_code ): 
+    async def disconnect(self,close_code): 
         # Quitter le groupe de notification utilisateur 
         await self.channel_layer.group_discard( 
             self.group_name, 
             self.channel_name 
         ) 
 
-    async  def  send_notification ( self, event ): 
-        notification = event[ 'message' ] 
+    async def send_notification(self, event): 
+        notification = event['message'] 
 
         # Envoyer un message à WebSocket 
         await self.send(text_data=json.dumps({ 
--- posey/notifications/routing.py
+++ posey/notifications/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from .consumers import NotificationConsumer
 
 websocket_urlpatterns = [ 
-    re_path('ws/notifications/(?P<user_id>\d+)/$' , NotificationConsumer.as_asgi()), 
+    re_path(r'ws/notifications/(?P<user_id>\d+)/$', NotificationConsumer.as_asgi()),
 ]
