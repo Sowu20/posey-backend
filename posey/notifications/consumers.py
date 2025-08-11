62f51bcae2eaf2a78139f90ce4b3bc3138169041
Sowu Kelly <sowukelly67@gmail.com>
2025-08-11 11:42:47 +0200

    posey_backend

--- posey/notifications/consumers.py
+++ posey/notifications/consumers.py
@@ -2,15 +2,15 @@ import json
 from channels.generic.websocket import AsyncWebsocketConsumer 
 
 class NotificationConsumer(AsyncWebsocketConsumer): 
-    async def connect(self): 
-        user = self.scope["user"]
-        if user.is_anonymous:
-            # Refuse la connexion si pas authentifié
-            await self.close()
-        else:
-            self.group_name = f"user_{user.id}"
-            await self.channel_layer.group_add(self.group_name, self.channel_name)
-            await self.accept()
+    async def connect(self):
+        self.group_name = f"user_{self.scope['user'].id}"
+
+        await self.channel_layer.group_add( 
+            self.group_name, 
+            self.channel_name 
+        )
+
+        await self.accept()
 
     async def disconnect(self,close_code): 
         # Quitter le groupe de notification utilisateur 
