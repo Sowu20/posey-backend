2391ea0678cb7c1d3f0c288ba22bbd222eb93978
Sowu Kelly <sowukelly67@gmail.com>
2025-08-13 11:22:22 +0200

    posey_backend

--- posey/prestations/serializers.py
+++ posey/prestations/serializers.py
@@ -89,9 +89,11 @@ class PrestationRefuseeSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class NotificationSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField()
+
     class Meta:
         model = Notification
-        fields = ['id', 'message', 'timestamp']
+        fields = ['id', 'message', 'timestamp', 'is_read']
 
 class PrestationClientSerializer(serializers.ModelSerializer):
     categorie = serializers.CharField(source='categorie.nom', read_only=True)
