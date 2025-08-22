07dae73a6742f16bb696cff4c83cdf732f2d4d8a
Sowu Kelly <sowukelly67@gmail.com>
2025-08-22 17:55:11 +0200

    posey_backend

--- posey/posey/settings.py
+++ posey/posey/settings.py
@@ -7,14 +7,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)kozn7!cqgydgx+2ow@5)eiby@&2=uyk-1g!84cbf4s2)-=zqs'
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "127.0.0.1:8000", "f5517475942e.ngrok-free.app", "posey-frontend-y8so.vercel.app"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "127.0.0.1:8000", "8933e772db4a.ngrok-free.app", "posey-frontend-y8so.vercel.app"]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://127.0.0.1:8000",
     "http://127.0.0.1:4040",
-    "https://f5517475942e.ngrok-free.app",
+    "https://8933e772db4a.ngrok-free.app",
     "https://posey-frontend-y8so.vercel.app",
 ]
 
