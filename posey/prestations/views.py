2d3f0aa197d8f17863fd405ec217b767979f4c3b
Sowu Kelly <sowukelly67@gmail.com>
2025-08-22 14:46:26 +0200

    posey_backend

--- posey/prestations/views.py
+++ posey/prestations/views.py
@@ -213,6 +213,10 @@ class AccepterPrestationView(APIView):
     def post(self, request, id):
         try:
             prestation = Prestation.objects.get(id=id)
+
+            prestation.prestataire = request.user
+            prestation.statut = 'accepte'
+            prestation.save()
             if prestation.accepte(request.user):
                 prestataire_username = prestation.prestataire.username if prestation.prestataire else "Un prestataire"
                 
