694c76c81e3caff78a759ca310de86dcc3005505
Sowu20 <sowukelly67@gmail.com>
2025-09-18 08:59:04 +0000

    posey-backend

--- posey/posey/settings.py
+++ posey/posey/settings.py
@@ -7,14 +7,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)kozn7!cqgydgx+2ow@5)eiby@&2=uyk-1g!84cbf4s2)-=zqs'
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "127.0.0.1:8000", "daughters-potential-mother-ssl.trycloudflare.com", "posey-frontend-y8so.vercel.app"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "127.0.0.1:8000", "spoke-handbook-single-lace.trycloudflare.com", "posey-frontend-y8so.vercel.app"]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://127.0.0.1:8000",
     "http://127.0.0.1:4040",
-    "https://daughters-potential-mother-ssl.trycloudflare.com",
+    "https://spoke-handbook-single-lace.trycloudflare.com",
     "https://posey-frontend-y8so.vercel.app",
 ]
 
