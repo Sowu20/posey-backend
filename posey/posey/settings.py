620a21e81933e81beed013843b0a56e611ea5825
Sowu20 <sowukelly67@gmail.com>
2025-09-25 09:55:22 +0000

    posey-backend

--- posey/posey/settings.py
+++ posey/posey/settings.py
@@ -7,14 +7,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)kozn7!cqgydgx+2ow@5)eiby@&2=uyk-1g!84cbf4s2)-=zqs'
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "127.0.0.1:8000", "mph-dave-publication-timeline.trycloudflare.com", "posey-frontend-y8so.vercel.app"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "127.0.0.1:8000", "francis-bundle-upgrades-chronicles.trycloudflare.com", "posey-frontend-y8so.vercel.app"]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://127.0.0.1:8000",
     "http://127.0.0.1:4040",
-    "https://mph-dave-publication-timeline.trycloudflare.com",
+    "https://francis-bundle-upgrades-chronicles.trycloudflare.com",
     "https://posey-frontend-y8so.vercel.app",
 ]
 
@@ -93,7 +93,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'sowukelly67@gmail.com'
-EMAIL_HOST_PASSWORD = 'sowukelly20'
+EMAIL_HOST_PASSWORD = 'rpwrrbwobgwryjue'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 FRONTEND_URL = 'https://posey-frontend-y8so.vercel.app'
