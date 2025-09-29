8dd4de38fd5f565cedb02f79c900550745232679
Sowu20 <sowukelly67@gmail.com>
2025-09-29 13:09:08 +0000

    posey-backend

--- posey/posey/settings.py
+++ posey/posey/settings.py
@@ -7,14 +7,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)kozn7!cqgydgx+2ow@5)eiby@&2=uyk-1g!84cbf4s2)-=zqs'
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "127.0.0.1:8000", "apply-engineering-rainbow-hosted.trycloudflare.com", "posey-frontend-y8so.vercel.app"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "127.0.0.1:8000", "renewable-usually-flood-topic.trycloudflare.com", "posey-frontend-y8so.vercel.app"]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://127.0.0.1:8000",
     "http://127.0.0.1:4040",
-    "https://apply-engineering-rainbow-hosted.trycloudflare.com",
+    "https://renewable-usually-flood-topic.trycloudflare.com",
     "https://posey-frontend-y8so.vercel.app",
 ]
 
