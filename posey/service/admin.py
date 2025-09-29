54fb1b8bc8f0229f23452ba102105c4711421d98
Sowu20 <sowukelly67@gmail.com>
2025-09-29 11:05:28 +0000

    posey-backend

--- posey/service/admin.py
+++ posey/service/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Service
 
 # Register your models here.
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('prestataire', 'categorie', 'nom', 'description', 'prix', 'date_creation', 'image')
+
+admin.site.register(Service, ServiceAdmin)
