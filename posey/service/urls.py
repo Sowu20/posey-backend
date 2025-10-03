0e658709d28f0d1ab58a8eff857a0f67c6d1efe9
Sowu20 <sowukelly67@gmail.com>
2025-10-03 13:22:20 +0000

    posey-backend

--- posey/service/urls.py
+++ posey/service/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import ServiceListCreateView, ServiceDetailView
+from .views import ServiceListCreateView, RegisterServiceView, ServiceDetailView, UpdateServiceView, DeleteServiceView
 
 urlpatterns = [
     path('mes_services/<int:prestataire_id>', ServiceListCreateView.as_view(), name='service-list-create'),
-    path('list_service/<int:id>/', ServiceDetailView.as_view(), name='service-detail')
+    path('register_service/', RegisterServiceView.as_view(), name='register-service'),
+    path('list_service/<int:id>/', ServiceDetailView.as_view(), name='service-detail'),
+    path('update_service/<int:id>/', UpdateServiceView.as_view(), name='update-service'),
+    path('delete_service/<int:id>/', DeleteServiceView.as_view(), name='delete-service')
 ]
--- posey/service/views.py
+++ posey/service/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics, permissions
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .models import Service
 from .serilizers import ServiceSerializer
 
@@ -16,9 +18,30 @@ class ServiceListCreateView(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save(prestataire=self.request.user)
 
+# Enregistrer un service
+class RegisterServiceView(generics.CreateAPIView):
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer 
+
 # Liste des services d'un prestataire
 class ServiceDetailView(generics.ListAPIView):
     serializer_class = ServiceSerializer
     def get_queryset(self):
         prestataire_id = self.kwargs.get("id")
         return Service.objects.filter(prestataire_id=prestataire_id).order_by("-date_creation")
+    
+# Modifier un service
+class UpdateServiceView(generics.UpdateAPIView):
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer 
+    lookup_field = 'id'
+
+# Supprimer un service
+class DeleteServiceView(APIView):
+    def delete(self, request, id, *args, **kwargs):
+        try:
+            service = Service.objects.get(id=id)
+            service.delete()
+            return Response({"Utilisateur supprimé avec succès."}, status=204)
+        except Service.DoesNotExist:
+            return Response({"Utilisateur introuvable."}, status=400)
